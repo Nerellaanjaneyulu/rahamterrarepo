61fab8bc4fb959922a9731770fdd8530c178302b
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-22 23:14:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "three" {
   count         = 2
   ami           = "ami-06fa3f12191aa3337"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
 
   tags = {
     Name = "test-server"
