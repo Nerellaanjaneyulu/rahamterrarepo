f1dcd6d8bf4da55ef75aa963318d3f874902a280
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-22 16:06:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "three" {
   count         = 2
-  ami           = "ami-0341d95f75f311023"
-  instance_type = "t2.micro"
+  ami           = "ami-06fa3f12191aa3337"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "test-server"
