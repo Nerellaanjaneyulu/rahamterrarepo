42c57e496db95e8ecf5fe662102f6a426e7f141f
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-24 15:49:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
   count         = 2
-  ami           = "ami-06fa3f12191aa3337"
+  ami           = "ami-0341d95f75f311023"
   instance_type = "t3.small"
 
   tags = {
